85a3a8c7b62bac1087b88c3cc08d1928d482fcaf
zmtmqhdl <zmtmqhdl@naver.com>
2025-02-17 23:38:25 +0900

    [update] BottomNavigation

--- presentation/src/main/java/com/example/presentation/component/DesignSystemBottomNavigation.kt
+++ presentation/src/main/java/com/example/presentation/component/DesignSystemBottomNavigation.kt
@@ -1,8 +1,11 @@
 package com.example.presentation.component
 
+import androidx.compose.material3.NavigationBar
 import androidx.compose.runtime.Composable
 
 @Composable
-fun PrimaryBottomNavigation() {
+fun PrimaryBottomNavigation(
 
+) {
+    NavigationBar {  }
 }
