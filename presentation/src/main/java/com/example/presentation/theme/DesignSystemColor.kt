18b3c4250b81596dc2e75a65e21fb1fe5e5d16cb
zmtmqhdl <zmtmqhdl@naver.com>
2025-02-20 23:55:48 +0900

    [update] theme

--- presentation/src/main/java/com/example/presentation/theme/DesignSystemColor.kt
+++ presentation/src/main/java/com/example/presentation/theme/DesignSystemColor.kt
@@ -1,14 +1,7 @@
 package com.example.presentation.theme
 
+import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
-
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
 
 object DesignSystemSingleColor {
     val White = Color(0xFFFFFFFF)
@@ -22,8 +15,12 @@ sealed class DesignSystemColor {
     abstract val background: Color
     abstract val outline: Color
 
+    private fun themeColor(light: Color, dark: Color): Color {
+        return if (isDarkTheme()) dark else light
+    }
+
     data object Primary : DesignSystemColor() {
-        override val fontColor = Color(0xFF067CFB)
+        override val fontColor get() = themeColor(Color(0xFF067CFB),Color(0xFF067CFB) )
         override val background = Color(0xFFC9E6FD)
         override val outline = Color(0xFFECF5FD)
     }
@@ -66,8 +63,122 @@ sealed class DesignSystemColor {
 
     data object Disable : DesignSystemColor() {
         override val fontColor = Color(0xFFFDD000)
-        override val background = Color(0xFFFDD000)
-        override val outline = Color(0xFFFDD000)
+        override val background = Color(0xFFFBF6C9)
+        override val outline = Color(0xFFFDFAE3)
     }
-
 }
+
+@Immutable
+data class ColorSet(
+
+
+    val material: ColorScheme,
+    val background: Color,
+    val white: Color = White,
+    val primary100: Color,
+    val primary30: Color,
+    val primary10: Color,
+    val primary5: Color,
+    val accent100: Color,
+    val accent10: Color,
+    val orange50: Color,
+    val yellow50: Color,
+    val gray90: Color,
+    val gray80: Color,
+    val gray70: Color,
+    val gray60: Color,
+    val gray50: Color,
+    val gray40: Color,
+    val gray30: Color,
+    val gray20: Color,
+    val gray10: Color,
+    val profile: Color,
+    val bgElevated: Color,
+    val dividerLine: Color,
+    val textPrimary: Color,
+    val textSecondary: Color,
+    val textInfo: Color,
+    val textDisabled: Color,
+    val iconDefault: Color,
+    val iconSecondary: Color,
+    val iconDisabled: Color,
+    val textFieldDefault: Color,
+    val textFieldDisabled: Color,
+    val textFieldBtnBase: Color,
+    val textFieldBtnText: Color,
+)
+//
+//internal val lightColorSet = ColorSet(
+//    material = lightColorScheme(),
+//    background = Background,
+//    primary100 = Primary100,
+//    primary30 = Primary30,
+//    primary10 = Primary10,
+//    primary5 = Primary05,
+//    accent100 = Accent,
+//    accent10 = AccentToast,
+//    orange50 = Noti,
+//    yellow50 = Favorite,
+//    gray90 = Gray90,
+//    gray80 = Gray80,
+//    gray70 = Gray70,
+//    gray60 = Gray60,
+//    gray50 = Gray50,
+//    gray40 = Gray40,
+//    gray30 = Gray30,
+//    gray20 = Gray20,
+//    gray10 = Gray10,
+//    profile = Profile,
+//    bgElevated = White,
+//    dividerLine = Gray10,
+//    textPrimary = Gray90,
+//    textSecondary = Gray60,
+//    textInfo = Gray50,
+//    textDisabled = Gray40,
+//    iconDefault = Gray60,
+//    iconSecondary = Gray50,
+//    iconDisabled = Gray40,
+//    textFieldDefault = White,
+//    textFieldDisabled = Gray10,
+//    textFieldBtnBase = Gray20,
+//    textFieldBtnText = White
+//)
+//
+//internal val darkColorSet = ColorSet(
+//    material = darkColorScheme(),
+//    background = BackgroundDark,
+//    primary100 = Primary100Dark,
+//    primary30 = PrimaryLightDark,
+//    primary10 = PrimaryBaseDark,
+//    primary5 = PrimaryToastDark,
+//    accent100 = AccentDark,
+//    accent10 = AccentToastDark,
+//    orange50 = NotiDark,
+//    yellow50 = FavoriteDark,
+//    gray90 = Gray90Dark,
+//    gray80 = Gray80Dark,
+//    gray70 = Gray70Dark,
+//    gray60 = Gray60Dark,
+//    gray50 = Gray50Dark,
+//    gray40 = Gray40Dark,
+//    gray30 = Gray30Dark,
+//    gray20 = Gray20Dark,
+//    gray10 = Gray10Dark,
+//    profile = ProfileDark,
+//    bgElevated = Gray10Dark,
+//    dividerLine = Gray20Dark,
+//    textPrimary = Gray80Dark,
+//    textSecondary = Gray60Dark,
+//    textInfo = Gray50Dark,
+//    textDisabled = Gray40Dark,
+//    iconDefault = Gray60Dark,
+//    iconSecondary = Gray50Dark,
+//    iconDisabled = Gray40Dark,
+//    textFieldDefault = Gray30Dark,
+//    textFieldDisabled = Gray20Dark,
+//    textFieldBtnBase = Gray40Dark,
+//    textFieldBtnText = Gray50Dark
+//)
+//
+//internal val LocalColorSet = androidx.compose.runtime.compositionLocalOf { lightColorSet }
+
