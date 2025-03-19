eb944c9d81b3d09e437f2eac8e4fd1ddc30875ec
zmtmqhdl <zmtmqhdl@naver.com>
2025-03-19 22:11:57 +0900

    [update] button clip

--- presentation/src/main/java/com/example/presentation/component/DesignSystemButton.kt
+++ presentation/src/main/java/com/example/presentation/component/DesignSystemButton.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,10 +41,12 @@ fun PrimaryButton(
                 Modifier
                     .width(width)
                     .height(height)
+                    .clip(DesignSystemTheme.shape.button)
             } else {
                 Modifier
                     .fillMaxWidth()
                     .height(height)
+                    .clip(DesignSystemTheme.shape.button)
             },
         enabled = state,
         shape = DesignSystemTheme.shape.button,
