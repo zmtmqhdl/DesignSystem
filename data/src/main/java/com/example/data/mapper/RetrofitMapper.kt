9e98881a50f045f76ebc3853c48a1fcf6a0c3b5d
zmtmqhdl <zmtmqhdl@naver.com>
2025-06-13 23:40:43 +0900

    [update] screen loading

--- data/src/main/java/com/example/data/mapper/RetrofitMapper.kt
+++ data/src/main/java/com/example/data/mapper/RetrofitMapper.kt
@@ -3,20 +3,19 @@ package com.example.data.mapper
 import com.example.data.model.RetrofitDto
 import com.example.domain.repository.RetrofitData
 
-fun RetrofitDto.toDomain(): RetrofitData {
-    return RetrofitData(
+fun RetrofitDto.toDomain(): RetrofitData =
+    RetrofitData(
         userId = this.userId,
         id = this.id,
         title = this.title,
         completed = this.completed
     )
-}
 
-fun RetrofitData.toDto(): RetrofitDto {
-    return RetrofitDto(
+
+fun RetrofitData.toDto(): RetrofitDto =
+    RetrofitDto(
         userId = this.userId,
         id = this.id,
         title = this.title,
         completed = this.completed
     )
-}
--- data/src/main/java/com/example/data/mapper/RoomMapper.kt
+++ data/src/main/java/com/example/data/mapper/RoomMapper.kt
@@ -3,10 +3,9 @@ package com.example.data.mapper
 import com.example.data.model.RoomEntity
 import com.example.domain.model.RoomData
 
-fun RoomEntity.toDomain(): RoomData {
-    return RoomData(id = id, name = name, age = age)
-}
+fun RoomEntity.toDomain(): RoomData =
+    RoomData(id = id, name = name, age = age)
 
-fun RoomData.toRoom(): RoomEntity {
-    return RoomEntity(id = id, name = name, age = age)
-}
+
+fun RoomData.toRoom(): RoomEntity =
+    RoomEntity(id = id, name = name, age = age)
