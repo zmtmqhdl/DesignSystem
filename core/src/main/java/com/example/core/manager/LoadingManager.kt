51442153a56e8dbe5431fc1f62ae875d6f08d9a3
zmtmqhdl <zmtmqhdl@naver.com>
2025-07-09 01:29:40 +0900

    [update] loading manager fix

--- core/src/main/java/com/example/core/manager/LoadingManager.kt
+++ core/src/main/java/com/example/core/manager/LoadingManager.kt
@@ -1,5 +1,7 @@
 package com.example.core.manager
 
+import android.os.Handler
+import android.os.Looper
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -7,18 +9,40 @@ import java.util.concurrent.atomic.AtomicInteger
 
 object LoadingManager {
     private val loadingCount = AtomicInteger(0)
-    private val _loading = MutableStateFlow<Boolean>(false)
+    private val _loading = MutableStateFlow(false)
     val loading: StateFlow<Boolean> = _loading.asStateFlow()
+
+    private var showTime: Long = 0L
+    private val minDurationMillis = 1000L  // 최소 로딩 시간
+
+    private val handler = Handler(Looper.getMainLooper())
 
     fun show() {
         if (loadingCount.incrementAndGet() == 1) {
+            showTime = System.currentTimeMillis()
             _loading.value = true
         }
     }
 
     fun hide() {
         if (loadingCount.decrementAndGet() == 0) {
-            _loading.value = false
+            val elapsed = System.currentTimeMillis() - showTime
+            if (elapsed >= minDurationMillis) {
+                _loading.value = false
+            } else {
+                val delay = minDurationMillis - elapsed
+                handler.postDelayed({
+                    if (loadingCount.get() == 0) {
+                        _loading.value = false
+                    }
+                }, delay)
+            }
         }
+    }
+
+    fun superHide() {
+        loadingCount.set(0)
+        handler.removeCallbacksAndMessages(null)
+        _loading.value = false
     }
 }
