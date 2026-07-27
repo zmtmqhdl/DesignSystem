c5d1eba140f24581d368a00478e8cccd555b75aa
zmtmqhdl <zmtmqhdl@naver.com>
2026-07-28 01:24:43 +0900

    Add signingConfig
    [Build List]
    - [ALL]

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,6 +45,7 @@ extensions.configure<ApplicationExtension> {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
